ea818d617a7b7e04ff7fb8a809b5194212d2bddc
xStef23 <trstefy@gmail.com>
2022-03-25 16:13:03 +0000

    added homepage

--- src/App.js
+++ src/App.js
@@ -13,11 +13,12 @@ function App() {
       <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/website" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-      <Home></Home>
+
       <Footer></Footer>
     </Router>
   );
