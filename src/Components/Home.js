e874a0c2e8efb635ad5d115a5c11cb920c0d7866
xStef23 <trstefy@gmail.com>
2022-03-29 13:14:07 +0100

    changed colours + fonts

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -30,20 +30,6 @@ export const Home = () => {
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item interval={1000}>
-          <img
-            className="c-image"
-            src="https://images.unsplash.com/photo-1537403005834-9824a1ce7b1b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-            alt="Third slide"
-          />
-
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
       </Carousel>
     </div>
   );
